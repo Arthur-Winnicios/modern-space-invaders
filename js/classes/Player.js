ff3158d05e15cab0c2ce31a7245445e1e5ad0339
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-05-23 16:22:27 -0400

    add draw function in Player class

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,24 +1,46 @@
 class Player {
-    constructor() {
-        this.velocity = {
-            x: 0,
-            y: 0
-        };
-        this.rotation = 0;
-        this.opacity = 1;
-        const image = new Image();
-        image.src = "./img/spaceship.png";
-        image.onload = () => {
-            const scale = 0.15
-            this.image = image;
-            this.width = image.width * scale;
-            this.height = image.height * scale;
-            this.position = {
-                x: canvas.width / 2 - this.width / 2,
-                y: canvas.height / 2 - this.height / 2
-            };
-        }
-        this.particles = [];
-        this.frames = 0;
-    }
+  constructor() {
+    this.velocity = {
+      x: 0,
+      y: 0
+    };
+    this.rotation = 0;
+    this.opacity = 1;
+    const image = new Image();
+    image.src = "./img/spaceship.png";
+    image.onload = () => {
+      const scale = 0.15;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height / 2 - this.height / 2
+      };
+    };
+    this.particles = [];
+    this.frames = 0;
+  }
+
+  draw() {
+    c.save();
+    c.globalAlpha = this.opacity;
+    c.translate(
+      player.position.x + player.width / 2,
+      player.position.y + player.height / 2
+    );
+    c.rotate(this.rotation);
+    c.translate(
+      -player.position.x - player.width / 2,
+      -player.position.y - player.height / 2
+    );
+    c.drawImage(
+      this.image,
+      player.position.x,
+      player.position.y,
+      this.width,
+      this.height
+    );
+    c.restore();
+  }
 }
