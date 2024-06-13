c6bb67c2ca4c38abe335373a7e88144c6911c779
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-06-13 17:41:23 -0400

    add update grids, position and state of invader grids, check collisions between invaders and player projectiles

--- index.js
+++ index.js
@@ -257,3 +257,84 @@ for (let i = projectiles.length - 1; i >= 0; i--) {
     projectile.update();
   }
 }
+
+grids.forEach((grid, gridIndex) => {
+  grid.update();
+
+  if (frames % 100 === 10 && grid.invaders.lenght > 0) {
+    grid.invaders[Math.floor(Math.random() * grid.invaders.lenght)].shoot(
+      invadersProjectiles
+    );
+  }
+
+  for (let i = grid.invaders.length - 1; i >= 0; i--) {
+    const invader = grid.invaders[i];
+    invader.update({ velocity: grid.velocity });
+
+    for (let j = bombs.length - 1; j >= 0; j--) {
+      const bomb = bombs[j];
+
+      const invaderRadius = 15;
+
+      if (
+        Math.hypot(
+          invader.position.x - bomb.position.x,
+          invader.position.y - bomb.position.y
+        ) <
+          invaderRadius + bomb.radius &&
+        bomb.active
+      ) {
+        score += 50;
+        scoreEl.innerHTML = score;
+
+        grid.invaders.splice(i, 1);
+        createScoreLabel({
+          object: invader,
+          score: 50
+        });
+
+        createParticles({
+          object: invader,
+          fades: true
+        });
+      }
+    }
+    projectiles.forEach((projectile, j) => {
+      if (
+        projectile.position.y - projectile.radius <=
+          invader.position.y + invader.heigth &&
+        projectile.position.x + projectile.radius >= invader.position.x &&
+        projectile.position.x - projectile.radius <=
+          invader.position.x + invader.width &&
+        projectile.position.y + projectile.radius >= invader.position.y
+      ) {
+        setTimeout(() => {
+          const invaderFound = grid.invader.find(
+            (invader2) => invader2 === invader
+          );
+          const projectileFound = projectiles.find(
+            (projectile2) => projectile2 === projectile
+          );
+
+          if (invaderFound && projectileFound) {
+            score += 100;
+            scoreEl.innerHTML = score;
+
+            createScoreLabel({
+              object: invader
+            });
+
+            createParticles({
+              object: invader,
+              fades: true
+            });
+
+            audio.explode.play();
+            grid.invaders.spliced(i, 1);
+            projectiles.splice(j, 1);' '
+          }
+        });
+      }
+    });
+  }
+});
